901c362ee0c7e39abc65345bd76f480b2deb2489
Parzival <ph15995534440@outlook.com>
2026-04-03 12:09:52 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://cmpt363-travelapp-high-fidelity-prototype.onrender.com',
         changeOrigin: true,
       }
     }
